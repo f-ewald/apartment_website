e1ca730c682141ea18239b55512d3492ca57a9d1
Friedrich Ewald <f-ewald@users.github.com>
2016-10-23 15:55:29 -0700

    added multiple controls

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,36 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
+var merge = require('merge-stream');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
 gulp.task('sass', function() {
-	return gulp.src('./sass/**/*.scss')
+	var bootstrap = gulp.src('node_modules/bootstrap-sass/assets/stylesheets/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+   		.pipe(gulp.dest('dist/bootstrap'));
+	var own_code = gulp.src('./sass/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 	    .pipe(gulp.dest('dist/css'))
 	    .pipe(connect.reload());
+	return merge(bootstrap, own_code);
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('watch', ['sass:watch']);
+gulp.task('html:watch', function() {
+	gulp.watch('./html/**/*.html', function() {
+		gulp.src('./html/**/*.html')
+			.pipe(gulp.dest('dist'))
+			.pipe(connect.reload());
+	});
+});
+
+gulp.task('watch', ['sass:watch', 'html:watch']);
 
 gulp.task('debug', ['dist', 'sass', 'connect', 'watch']);
 
@@ -32,6 +45,11 @@ gulp.task('connect', function() {
 
 gulp.task('dist', function () {
 	// Distribution
-	gulp.src('index.html')
+	var html = gulp.src('html/**/*.html')
 		.pipe(gulp.dest('dist'));
+
+	var images = gulp.src('img/**/*.jpg')
+		.pipe(gulp.dest('dist/img'));
+
+	return [html, images];
 });
